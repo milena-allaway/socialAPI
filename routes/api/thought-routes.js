c49d807cefa22ad7a6ef96bda1183b43bd8671c7
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-12 00:18:34 -0500

    added thought-routes.js code for all thoughts/reaction routes

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -10,4 +10,27 @@ const {
     removeReactionById
 } = require('../../controllers/thought-controller');
 
+// /api/thought
+// get all thoughts and create a thought
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thought/:thoughtId
+// get one thought, update one thought, and delete one thought
+router.route('/:thoughtId')
+    .get(getThoughtById)
+    .put(updateThoughtById)
+    .delete(deleteThoughtById);
+
+// /api/thought/:thoughtId/reactions
+// add a reaction
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// /api/thought/:thoughtId/reactions/:reactionId
+// remove a reaction
+router.route('/:thoughtId/reactions/:reactionId')
+    .delete(removeReactionById);
+
 module.exports = router;
