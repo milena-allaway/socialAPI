e4611dfaed57b630f250abcf7ec5c7ad48ee7d49
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-11-14 23:26:20 -0500

    fixed bug

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -36,8 +36,7 @@ module.exports = {
     // POST a new user
     async createUser(req, res) {
         try {
-            const user = await User.create(req.body)
-                .select('-__v');
+            const user = await User.create(req.body);
             res.json(user);
         } catch (err) {
             res.status(500).json(err);
